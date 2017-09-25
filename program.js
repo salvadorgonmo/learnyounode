90c58a1dd33b742b3586f58ea6698f8a76074b7d
salvador_gonzalez <salvadorgonmo@gmail.com>
2017-09-24 22:32:04 -0500

    adding exercise 13 and finished

--- program.js
+++ program.js
@@ -299,4 +299,42 @@ http.createServer(connect).listen(port);
 */
 
 
+//exercise 13 //
+/*
+var http = require('http'),
+url = require('url'),
+port = process.argv[2];
 
+
+var connect = function (req, res) {
+
+var parsed = url.parse(req.url, true),
+    date = new Date(parsed.query.iso),
+    result = '';
+
+if (/api\/parsetime/.test(req.url)) {
+
+    result = {
+        hour: date.getHours(),
+        minute: date.getMinutes(),
+        second: date.getSeconds()
+    };
+
+} else if (/api\/unixtime/.test(req.url)) {
+    
+    result = {
+        unixtime: date.getTime()
+    };
+}
+
+res.writeHead(200, {
+    'Content-Type': 'application/json'
+});
+
+return res.end(JSON.stringify(result));
+};
+
+http.createServer(connect).listen(port);
+
+//
+*/
