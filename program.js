f0319ffde1ee025cdf46ec4a6cdb43f17341a49c
salvador_gonzalez <salvadorgonmo@gmail.com>
2017-09-24 22:26:53 -0500

    adding exercise 12

--- program.js
+++ program.js
@@ -272,9 +272,31 @@ server.listen(Number(process.argv[2]))
 //
 
 */
+/*
+
+// exercise 12 //
+var map = require('through2-map'),
+http = require('http'),
+port = process.argv[2];
 
 
+var upcase = function (chunk) {
 
+return chunk.toString().toUpperCase();
+};
+
+
+var connect = function (req, res) {
+
+if (req.method === 'POST') {
+    return req.pipe(map(upcase)).pipe(res);
+}
+};
+
+http.createServer(connect).listen(port);
+
+
+*/
 
 
 
